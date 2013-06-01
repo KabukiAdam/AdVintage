64a73860199ff5033aa979c25920269e6659680b
Adam Shaw <adam@kabukivision.com>
2013-06-01 12:28:40 +0800

    Initial commit

---
+++ AdVintage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AdVintage
+//
+//  Created by Adam Shaw on 1/06/13.
+//  Copyright (c) 2013 AdVintage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
