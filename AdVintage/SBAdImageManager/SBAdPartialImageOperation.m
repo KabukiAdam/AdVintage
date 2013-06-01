40853b9ed5de3fef98d8dbe0b9e53e1d746f10fa
Simon Burbidge <simon@whiskit.com.au>
2013-06-01 23:32:52 +0800

    Removed Git remnants.

--- AdVintage/SBAdImageManager/SBAdPartialImageOperation.m
+++ AdVintage/SBAdImageManager/SBAdPartialImageOperation.m
@@ -46,12 +46,7 @@
         
 
         while (!finished) {
-<<<<<<< HEAD
             [NSThread sleepForTimeInterval:0.1f];
-=======
-            [NSThread sleepForTimeInterval:0.1];
-
->>>>>>> 3efab8f94104d85445605e4ef24d0f11dc43478f
         }
         [(NSObject*)delegate performSelectorOnMainThread:@selector(adPartialImageOperationDidFinish:) withObject:self waitUntilDone:NO];
     }
