e4a3aabbbc6a831a889999f609078f9cbfa68efd
Adam Shaw <adam@kabukivision.com>
2013-06-02 13:52:52 +0800

    Favorites (not complete)

---
+++ AdVintage/FavoritesManager.h
@@ -0,0 +1,21 @@
+//
+//  FavoritesManager.h
+//  AdVintage
+//
+//  Created by Adam Shaw on 2/06/13.
+//  Copyright (c) 2013 AdVintage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Article.h"
+
+@interface FavoritesManager : NSObject
+
+@property (strong, nonatomic) NSMutableDictionary *favoriteArticles;
+
++ (FavoritesManager *)sharedInstance;
+
+- (void)favoriteArticle:(Article *)article;
+- (void)unfavoriteArticle:(Article *)article;
+
+@end
---
+++ AdVintage/FavoritesManager.m
@@ -0,0 +1,65 @@
+//
+//  FavoritesManager.m
+//  AdVintage
+//
+//  Created by Adam Shaw on 2/06/13.
+//  Copyright (c) 2013 AdVintage. All rights reserved.
+//
+
+#import "FavoritesManager.h"
+
+#define FAVORITES_KEY @"FAVORITES"
+
+
+@implementation FavoritesManager
+
+static FavoritesManager *sharedObject = nil;
+
++ (FavoritesManager *)sharedInstance
+{
+    static dispatch_once_t _singletonPredicate;
+    
+    dispatch_once(&_singletonPredicate, ^{
+        sharedObject = [[super allocWithZone:nil] init];
+        sharedObject.favoriteArticles = [NSMutableDictionary dictionary];
+    });
+    
+    return sharedObject;
+}
+
+- (void)favoriteArticle:(Article *)article
+{
+    [self.favoriteArticles setObject:article forKey:@(article.articleID)];
+    [self saveFavorites];
+}
+
+- (void)unfavoriteArticle:(Article *)article
+{
+    [self.favoriteArticles removeObjectForKey:@(article.articleID)];
+    [self saveFavorites];
+}
+
+- (void)loadFavorites
+{
+    NSArray *favArray = [[NSUserDefaults standardUserDefaults] objectForKey:FAVORITES_KEY];
+    [self.favoriteArticles removeAllObjects];
+    for (NSDictionary *artDict in favArray)
+    {
+        Article *article = [[Article alloc] initWithDictionary:artDict];
+        [self.favoriteArticles setObject:article forKey:@(article.articleID)];
+    }
+}
+
+- (void)saveFavorites
+{
+    NSMutableArray *prefArray = [NSMutableArray array];
+    for (Article *article in self.favoriteArticles)
+    {
+        NSDictionary *artDict = [article getDictionary];
+        [prefArray addObject:artDict];
+    }
+    [[NSUserDefaults standardUserDefaults] setObject:prefArray forKey:FAVORITES_KEY];
+}
+
+
+@end
